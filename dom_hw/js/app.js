96c33f7571db58258a01c5b33b168fcffed0773d
Ewan Brain <ewan.brain@gmail.com>
2021-01-10 16:05:47 +0000

    added to html, starting on app.js

--- dom_hw/js/app.js
+++ dom_hw/js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const newItemForm = document.querySelector('#new-item-form');
+    newItemForm.addEventListener('submit', handleNewItemFormSubmit);
 
+    
 
 });

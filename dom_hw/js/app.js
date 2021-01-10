6ad435029c3abc12def2aa0169301fca3c5143d5
Ewan Brain <ewan.brain@gmail.com>
2021-01-10 16:27:25 +0000

    data appending to list

--- dom_hw/js/app.js
+++ dom_hw/js/app.js
@@ -15,3 +15,26 @@ const handleNewItemFormSubmit = function (event) {
 
     event.target.reset();
 }
+
+const createBandListItem = function (form) {
+    const bandListItem = document.createElement('li');
+    bandListItem.classList.add('band-list-item');
+
+    const name = document.createElement('h2');
+    name.textContent = form.name.value;
+    bandListItem.appendChild(name);
+
+    const genre = document.createElement('p');
+    genre.textContent = form.genre.value;
+    bandListItem.appendChild(genre);
+
+    const country = document.createElement('p');
+    country.textContent = form.country.value;
+    bandListItem.appendChild(country);
+
+    const rating = document.createElement('p');
+    rating.textContent = form.rating.value;
+    bandListItem.appendChild(rating);
+
+    return bandListItem;
+}

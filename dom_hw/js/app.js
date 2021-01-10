01b2fbb538364909141b2d057e8a730c07feabe7
Ewan Brain <ewan.brain@gmail.com>
2021-01-10 16:09:45 +0000

    added form submit function

--- dom_hw/js/app.js
+++ dom_hw/js/app.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const newItemForm = document.querySelector('#new-item-form');
     newItemForm.addEventListener('submit', handleNewItemFormSubmit);
 
-    
+
 
 });
+
+const handleNewItemFormSubmit = function (event) {
+    event.preventDefault();
+
+    const bandListItem = createBandListItem(event.target);
+    const bandList = document.querySelector('#band-list');
+    bandList.appendChild(bandListItem);
+
+    event.target.reset();
+}
